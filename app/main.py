9da0352d169547fe77b7dc3a722a078978ae69c5
martinristow <martinristov084@gmail.com>
2024-12-15 23:52:53 +0100

    Refactor main.py to integrate user routes
    - Commented out database model creation in `main.py`
    - Imported and included `user` router from `app.routes`
    - Simplified code by separating route logic into the `user` module

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from app import models
-from .database import engine
+# from app import models
+# from .database import engine
+from app.routes import user
 
-
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+app.include_router(user.router)
 
 
 @app.get("/")
