97445b30413fbb33112884bae4a660d158043a73
martinristow <martinristov084@gmail.com>
2025-01-14 23:54:29 +0100

    include authentication routes in the app
    - Updated `main.py` to include the `auth` router for authentication endpoints.
    - Ensured that both `user` and `auth` routes are now part of the FastAPI application.

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 # from app import models
 # from .database import engine
-from app.backend.routes import user
+from app.backend.routes import user, auth
 
 # models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
