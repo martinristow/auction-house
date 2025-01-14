d4d672b28f6eb8e84409c67fed7fefffae6be619
martinristow <martinristov084@gmail.com>
2025-01-14 23:53:45 +0100

    refactor(oauth2): enhance JWT authentication with token verification and user retrieval
    - Updated the `create_access_token` function for generating JWTs with a dynamic expiration time.
    - Added `verify_access_token` function to decode and validate JWTs, ensuring token integrity and extracting user ID.
    - Introduced `get_current_user` function to retrieve the current authenticated user from the database using the token.
    - Configured `OAuth2PasswordBearer` with the `/login` endpoint for token-based authentication.
    - Replaced direct `Settings` usage with the `settings` object from `config`.

--- app/backend/oauth2.py
+++ app/backend/oauth2.py
@@ -3,11 +3,17 @@ from datetime import datetime, timedelta, timezone
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jwt.exceptions import InvalidTokenError
-from .config import Settings
+from .config import settings
+from . import models, database
+from .schemas import token_schemas
+from sqlalchemy.orm import Session
 
-SECRET_KEY = Settings.secret_key
-ALGORITHM = Settings.algorithm
-ACCESS_TOKEN_EXPIRE_MINUTES = Settings.access_token_expire_minutes
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 
 def create_access_token(data: dict):
@@ -19,3 +25,27 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        return token_schemas.TokenData(id=id)
+    except InvalidTokenError:
+        raise credentials_exception
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(token_data.id == models.User.id).first()
+    if user is None:
+        raise credentials_exception
+    return user
