0914426cd7e8423ee046688270c9faa581618eae
martinristow <martinristov084@gmail.com>
2025-01-15 16:47:44 +0100

    Add current user

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
 # from app import models
 # from .database import engine
-from app.backend.routes import user, auth
-
+from app.backend.routes import user, auth, auction
+from fastapi.middleware.cors import CORSMiddleware
 # models.Base.metadata.create_all(bind=engine)
+
+
+
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Дозволи пристап од сите домени
+    allow_credentials=True,
+    allow_methods=["*"],  # Дозволи сите HTTP методи
+    allow_headers=["*"],  # Дозволи сите HTTP хедери
+)
+
+
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(auction.router)
 
 
 @app.get("/")
