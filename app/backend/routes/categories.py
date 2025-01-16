e764a9bedc07267ccd69f5388bf2a8fbba06acf4
martinristow <martinristov084@gmail.com>
2025-01-16 19:55:54 +0100

    Add categories endpoints for active and closed auctions
    - Created a new `categories.py` file with two endpoints:
      - `/categories/active-auctions`: Retrieves all active auctions.
      - `/categories/closed-auctions`: Retrieves all closed auctions.
    - Added logic to query auctions based on their active status and return them in descending order of IDs.
    - Included error handling for cases where no auctions are found.

---
+++ app/backend/routes/categories.py
@@ -0,0 +1,31 @@
+from fastapi import status, Depends, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+from ..database import get_db
+from app.backend import models
+from ..schemas import auction_schemas
+from typing import List
+
+
+router = APIRouter(tags=["Categories"])
+
+
+@router.get("/categories/active-auctions", response_model=List[auction_schemas.AuctionOut])
+async def get_all_categories(db: Session = Depends(get_db)):
+
+    active_auctions = db.query(models.Auction).filter("true" == models.Auction.is_active).order_by(models.Auction.id.desc()).all()
+
+    if active_auctions is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This moment we do not have any auctions!")
+
+    return active_auctions
+
+
+@router.get("/categories/closed-auctions", response_model=List[auction_schemas.AuctionOut])
+async def get_all_categories(db: Session = Depends(get_db)):
+
+    closed_auctions = db.query(models.Auction).filter("false" == models.Auction.is_active).order_by(models.Auction.id.desc()).all()
+
+    if closed_auctions is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This moment we do not have any auctions!")
+
+    return closed_auctions
