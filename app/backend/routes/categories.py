b9a1c5c98357834f206edeebbcda86c3d44d394b
martinristow <martinristov084@gmail.com>
2025-01-16 20:03:01 +0100

    Update categories.py to add get_all_categories endpoint
    - Added `get_all_categories` endpoint to fetch all auctions.
    - Returns all auctions ordered by ID in descending order.
    - Updated HTTP exception handling for when no auctions are found.

--- app/backend/routes/categories.py
+++ app/backend/routes/categories.py
@@ -10,7 +10,7 @@ router = APIRouter(tags=["Categories"])
 
 
 @router.get("/categories/active-auctions", response_model=List[auction_schemas.AuctionOut])
-async def get_all_categories(db: Session = Depends(get_db)):
+async def get_all_active_auctions(db: Session = Depends(get_db)):
 
     active_auctions = db.query(models.Auction).filter("true" == models.Auction.is_active).order_by(models.Auction.id.desc()).all()
 
@@ -21,7 +21,7 @@ async def get_all_categories(db: Session = Depends(get_db)):
 
 
 @router.get("/categories/closed-auctions", response_model=List[auction_schemas.AuctionOut])
-async def get_all_categories(db: Session = Depends(get_db)):
+async def get_all_closed_auctions(db: Session = Depends(get_db)):
 
     closed_auctions = db.query(models.Auction).filter("false" == models.Auction.is_active).order_by(models.Auction.id.desc()).all()
 
@@ -29,3 +29,15 @@ async def get_all_categories(db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This moment we do not have any auctions!")
 
     return closed_auctions
+
+
+
+@router.get("/categories/all", response_model=List[auction_schemas.AuctionOut])
+async def get_all_categories(db: Session = Depends(get_db)):
+
+    all_auctions = db.query(models.Auction).order_by(models.Auction.id.desc()).all()
+
+    if all_auctions is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="This moment we do not have any auctions!")
+
+    return all_auctions
