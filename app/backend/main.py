7782f4e5ea624c429abf36dc3a45f62ef6fc8ec4
martinristow <martinristov084@gmail.com>
2025-01-18 01:40:19 +0100

    Enhance main.py with periodic background task for auction updates
    Imported the aukcii function for auction status updates.
    Added a periodic background task to automatically check and update auction statuses every 10 seconds using asyncio.
    Included the get_db function to retrieve database sessions for the periodic task.
    Registered the periodic task to start during the application startup with @app.on_event("startup").
    Updated code comments for clarity and functionality.

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-# from app import models
-# from .database import engine
 from app.backend.routes import user, auth, auction, bid, categories
 from fastapi.middleware.cors import CORSMiddleware
+from app.backend.routes.bid import aukcii
+from app.backend.database import get_db
+import asyncio
+
 # models.Base.metadata.create_all(bind=engine)
 
 
@@ -28,6 +30,20 @@ app.include_router(bid.router)
 app.include_router(categories.router)
 
 
+async def periodic_task():
+    while True:
+        db = next(get_db())
+        await aukcii(db=db)
+        await asyncio.sleep(10)
+
+
+@app.on_event("startup")
+async def start_background_tasks():
+    asyncio.create_task(periodic_task())
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
