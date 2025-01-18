227dd2355df01f2445331a42fb29b1a6f9758f80
martinristow <martinristov084@gmail.com>
2025-01-18 20:30:32 +0100

    Update config.py to include email configuration
    Added my_email and password fields to the Settings class for email functionality.
    Updated the configuration to load email-related credentials from the .env file.

--- app/backend/config.py
+++ app/backend/config.py
@@ -10,6 +10,8 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
+    my_email: str
+    password: str
 
     class Config:
         env_file = ".env"
